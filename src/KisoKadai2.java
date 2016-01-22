66f2bb9712159c24885e2ee8d684f2dc44b872dd
yuki hoshino <hoshih0sh1@outlook.jp>
2016-01-22 13:39:15 +0900

    kisokadai@yuki.hoshino20160122

--- src/KisoKadai2.java
+++ src/KisoKadai2.java
@@ -8,22 +8,28 @@ public class KisoKadai2 {
 	 * @param args
 	 */
 	public static void main(String[] args){
-		System.out.println("数字を入力してください");
-		@SuppressWarnings("resource")
-		int input = new java.util.Scanner(System.in). nextInt();
-		int ans = 50;
-		for(int i = 1; i <= 100; i++){
-			if(i == ans && i == input){
-				System.out.println("True!");
-				break;
-			}else if(input <= ans && i != input && i == ans){
-				System.out.println("小さいです");
-				break;
-			}else if(input >= ans && i != input && i == ans){
-				System.out.println("大きいです");
-				break;
+		System.out.println("スタート");
+		System.out.println("数字を入力してください \n ターン数が100になるまでに当ててください");
+		int a = 0;
+		int ans = new java.util.Random() .nextInt(100);
+			for(int i = 1; i <= 100; i++){
+				@SuppressWarnings("resource")
+				int input = new java.util.Scanner(System.in). nextInt();
+				if(ans == input){
+					System.out.println("True!");
+					break;
+				}else if(ans != input){
+					System.out.println("False...");
+				}
+
+				if(input > ans){
+					System.out.println("ヒント：小さいです");
+				}else if(input < ans){
+					System.out.println("ヒント：大きいです");
+				}
+				a++;
+				System.out.println("現在のターン数 : " + a);
 			}
-		}
 	}
 
 }
