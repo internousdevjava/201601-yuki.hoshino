1861eb8ee492f60f44d8d89865e1b54c84538f79
yuki hoshino <hoshih0sh1@outlook.jp>
2016-01-25 09:54:17 +0900

    KisoKadai3Classver

--- src/KisoKadai3Main.java
+++ src/KisoKadai3Main.java
@@ -2,16 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- *
- */
-
-/**
- * @author internous
- *
- */
 public class KisoKadai3Main {
-
 	/**
 	 * @param args
 	 * @throws IOException
@@ -22,11 +13,12 @@ public class KisoKadai3Main {
 		KisoKadai3Mkdir d = new KisoKadai3Mkdir();
 		KisoKadai3Out o = new KisoKadai3Out();
 		KisoKadai3Mkfile f = new KisoKadai3Mkfile();
+		KisoKadai3Dlt dl = new KisoKadai3Dlt();
 
 		int end = 0;
 		do{
 
-			System.out.println("\n\n--メニュー--\n\n1:フォルダを作成する\n2:ファイルを作成する\n3:ファイルに追記する\n4:ファイルを出力する\n99:終了\nのどれかをキー入力してください");
+			System.out.println("\n\n--メニュー--\n\n1:フォルダを作成する\n2:ファイルを作成する\n3:ファイルに追記する\n4:ファイルを出力する\n5:ファイル、フォルダを削除\n99:終了\nのどれかをキー入力してください");
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			String str = br.readLine();
 			int nu = 0;
@@ -54,13 +46,16 @@ public class KisoKadai3Main {
 			if(nu == 3){
 				System.out.println("ファイルに追記");
 				i.fdN();
-				i.input();
 				i.inp();
 			}
 			if(nu == 4){
 				System.out.println("ファイルを出力");
 				o.fdN();
 				o.out();
+			}
+			if(nu == 5){
+				System.out.println("ファイル、フォルダを削除");
+				dl.Dlt();
 			}
 		}while(end < 1);
 		System.out.println("--処理終了--");
--- src/KisoKadai3Out.java
+++ src/KisoKadai3Out.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,31 +10,45 @@ public class KisoKadai3Out {
 		System.out.println("ファイルを出力します。");
 		System.out.println("フォルダを指定してください");
 		this.dirN = new java.util.Scanner(System.in) .nextLine();
-		System.out.println("出力するファイル名を入力してください");
-		this.fileN = new java.util.Scanner(System.in) .nextLine();
-	}
-	void out(){
-		FileReader fileR = null;
-		try{
-			fileR = new FileReader(this.dirN + this.fileN);
-			System.out.println(this.dirN + "の" + this.fileN + "を出力します");
-		  	int ch;
-			while((ch = fileR.read()) != -1){
-			  System.out.print((char)ch);
-			}
 
-		}catch(FileNotFoundException e){
-			  System.out.println(e);
-		}catch(IOException e){
-			  System.out.println(e);
-		}finally{
-			if(fileR != null){
+	}
+	@SuppressWarnings("resource")
+	void out(){
+		File dirC = new File(this.dirN);
+		if (dirC.exists()){
+			System.out.println("出力するファイル名を入力してください");
+			this.fileN = new java.util.Scanner(System.in) .nextLine();
+			File fileC = new File(this.dirN +"\\" + this.fileN);
+			if (fileC.exists()){
+				FileReader fileR = null;
 				try{
-					if(fileR != null){
-						fileR.close();
+					fileR = new FileReader(this.dirN +"\\"+ this.fileN);
+					System.out.println(this.dirN + "の" + this.fileN + "を出力します");
+				  	int ch;
+					while((ch = fileR.read()) != -1){
+					  System.out.print((char)ch);
 					}
-				}catch(IOException e2){}
+
+				}catch(FileNotFoundException e){
+					  System.out.println(e);
+				}catch(IOException e){
+					  System.out.println(e);
+				}finally{
+					if(fileR != null){
+						try{
+							if(fileR != null){
+								fileR.close();
+							}
+						}catch(IOException e2){}
+					}
+				}
+			}else{
+			    System.out.println("ファイルは存在しません。メニューに戻ります。");
+			    return;
 			}
+		}else{
+		    System.out.println("フォルダは存在しません。メニューに戻ります。");
+		    return;
 		}
 	}
 }
