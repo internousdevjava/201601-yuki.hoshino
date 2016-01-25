7d9b7b653606513dedab928eab00b0bd8ea44d7e
yuki hoshino <hoshih0sh1@outlook.jp>
2016-01-25 17:08:32 +0900

    x、yに記述できる数字の範囲を1～9までに制限しました（22行目～33行目） 36行目に「|」を加えて、列を揃えました

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -15,20 +15,28 @@ public class KisoKadai1 {
 	 */
 	public static void main(String[] args){
 		final DecimalFormat df = new DecimalFormat("00");
-		System.out.println("数字Xを入力してください");
+		System.out.println("1～9までの数字Xを入力してください");
 		System.out.print("X=");
 		@SuppressWarnings("resource")
 		int x = new java.util.Scanner(System.in). nextInt();
-		System.out.println("数字Yを入力してください");
+		if(x > 9){
+			System.out.println("1～9までの範囲の数字を入力してください");
+			return;
+		}
+		System.out.println("1～9までの数字Yを入力してください");
 		System.out.print("Y=");
 		@SuppressWarnings("resource")
 		int y = new java.util.Scanner(System.in). nextInt();
+		if(y > 9){
+			System.out.println("1～9までの範囲の数字を入力してください");
+			return;
+		}
 		for(int i = 1; i <= x; i++){
 			for(int j = 1; j <= y; j++){
-				System.out.print(" "+ df.format(i*j));
+				System.out.print("|" + j + "*" + i + "="+ df.format(i*j) + "|");
 				System.out.print(" ");
 			}
-			System.out.println("");
+			System.out.println(" ");
 		}
 	}
 }
