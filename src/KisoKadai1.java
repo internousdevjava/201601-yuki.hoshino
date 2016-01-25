93cacc4e94c8b7e6fd4549b0a7a26ac130cf8f6a
yuki hoshino <hoshih0sh1@outlook.jp>
2016-01-25 14:36:59 +0900

    KisoKadai1にDecimalFormatを追記し、行と列を揃えました@yuki.hoshino

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 /**
  *
  */
@@ -12,19 +14,21 @@ public class KisoKadai1 {
 	 * @param args
 	 */
 	public static void main(String[] args){
-		System.out.println("数字を入力してください");
+		final DecimalFormat df = new DecimalFormat("00");
+		System.out.println("数字Xを入力してください");
+		System.out.print("X=");
 		@SuppressWarnings("resource")
 		int x = new java.util.Scanner(System.in). nextInt();
-		System.out.println("もう一度数字を入力してください");
+		System.out.println("数字Yを入力してください");
+		System.out.print("Y=");
 		@SuppressWarnings("resource")
 		int y = new java.util.Scanner(System.in). nextInt();
 		for(int i = 1; i <= x; i++){
 			for(int j = 1; j <= y; j++){
-				System.out.print(i * j);
+				System.out.print(" "+ df.format(i*j));
 				System.out.print(" ");
 			}
 			System.out.println("");
 		}
 	}
-
 }
