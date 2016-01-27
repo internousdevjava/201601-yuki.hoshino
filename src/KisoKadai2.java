432871c4743c3515cf00babfcd6e0320aa1677a3
yuki hoshino <hoshih0sh1@outlook.jp>
2016-01-27 10:07:22 +0900

    KisoKadai2改良版

--- src/KisoKadai2.java
+++ src/KisoKadai2.java
@@ -10,26 +10,37 @@ public class KisoKadai2 {
 	public static void main(String[] args){
 		System.out.println("スタート");
 		System.out.println("数字を入力してください \n ターン数が100になるまでに当ててください");
+
 		int a = 0;
 		int ans = new java.util.Random() .nextInt(100);
 			for(int i = 1; i <= 100; i++){
-				@SuppressWarnings("resource")
-				int input = new java.util.Scanner(System.in). nextInt();
-				if(ans == input){
-					System.out.println("True!");
-					break;
-				}else if(ans != input){
-					System.out.println("False...");
+
+			    try {
+			    	@SuppressWarnings("resource")
+					String x = new java.util.Scanner(System.in) .nextLine();
+					long input;
+				    input = Integer.parseInt(x);
+			    	if(ans == input){
+						System.out.println("True!");
+						break;
+					}else if(ans != input){
+						System.out.println("False...");
+					}
+
+					if(input > ans){
+						System.out.println("ヒント：答えは入力した数より小さいです");
+					}else if(input < ans){
+						System.out.println("ヒント：答えは入力した数より大きいです");
+					}else {
+
+					}
+				} catch (NumberFormatException e) {
+
+				    System.out.println("数字を半角英数で入れてください!");
 				}
 
-				if(input > ans){
-					System.out.println("ヒント：答えは入力した数より小さいです");
-				}else if(input < ans){
-					System.out.println("ヒント：答えは入力した数より大きいです");
-				}
 				a++;
 				System.out.println("現在のターン数 : " + a);
 			}
 	}
-
 }
